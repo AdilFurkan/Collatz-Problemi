fc4dd2cf60bb173f95092496a4d277a554fcbafb
AdilFurkan <56627313+AdilFurkan@users.noreply.github.com>
2019-10-28 01:16:58 +0300

    Add files via upload

---
+++ CollatzFork.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <unistd.h>
+ 
+int main()
+{
+    int sayi = 0;
+    int pid,status;
+ 
+    printf("Lütfen pozitif bir sayı giriniz : ");
+    scanf("%d", &sayi);
+ 
+       if(sayi < 0)
+    {
+          printf("Lütfen pozitif sayı giriniz! \n");
+      scanf("%d", &sayi);
+    }
+ 
+    pid = fork();
+ 
+    if(pid < 0)
+    {
+      printf("Child process yaratılamadı \n");
+      exit(-1);
+    }
+ 
+    else if(pid == 0)
+    {
+             do
+        {
+        if(sayi%2 != 0)
+        {
+          sayi = (sayi*3)+1;
+                }
+         
+        else if(sayi%2 == 0)
+        {
+          sayi = sayi/2;
+        }
+ 
+        printf("%d \n",sayi);
+        }while(sayi != 1);
+    }
+ 
+    
+    
+    else {
+        printf("pid %d \n",pid);
+    printf("Child process'in bitmesi bekleniyor.. \n");
+      wait(&status);
+          } 
+     
+     
+return 0;   
+}
